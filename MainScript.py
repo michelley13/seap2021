5829ae4331a0881615329e339143180787397231
michelley13 <86005708+michelley13@users.noreply.github.com>
2021-08-04 12:43:02 -0400

    Add files via upload

---
+++ MainScript.py
@@ -0,0 +1,38 @@
+
+#--------FILE1-TAGGING--------
+#string_to_add_for_file1 = "(FILE1)"
+#file1 = ("DL_UASM_LP7.mgf")
+#with open(file1, 'r') as f:
+#    file_lines = [''.join(  [string_to_add_for_file1, x.strip(),'\n']    ) for x in f.readlines()]
+
+#with open(file1, 'w') as f:
+#    f.writelines(file_lines)
+
+#--------FILE2-TAGGING--------
+#string_to_add_for_file2 = "(FILE2)"
+#file2 = ("DL_UASM_LP8.mgf")
+#with open(file2, 'r') as f:
+#    file_lines = [''.join(  [string_to_add_for_file2, x.strip(),'\n']    ) for x in f.readlines()]
+
+#with open(file2, 'w') as f:
+#    f.writelines(file_lines)
+
+#--------FILE3-TAGGING--------
+#string_to_add_for_file3 = "(FILE3)"
+#file3 = ("DL_UASM_LP9.mgf")
+#with open(file3, 'r') as f:
+#    file_lines = [''.join(  [string_to_add_for_file3, x.strip(),'\n']    ) for x in f.readlines()]
+
+#with open(file3, 'w') as f:
+#    f.writelines(file_lines)
+
+#--------MERGE-FILES--------
+#combofile = ["DL_UASM_LP7.mgf", "DL_UASM_LP8.mgf", "DL_UASM_LP9.mgf"]
+#with open('DL_LP789_CombinedFile', 'w') as outfile:
+#    for fname in combofile:
+#        with open(fname) as infile:
+#            outfile.write(infile.read())
+
+#--------SORTING-M/Z--------
+
+ 
